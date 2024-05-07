6725880daef08663139e653df436244d74a1d592
geraldkisilu <muligerald6@gmail.com>
2024-05-08 01:35:28 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React, { useState, useEffect } from 'react';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import HomePage from './Components/HomePage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './Components/HomePage';
 import "./App.css";
 import PeopleList from './Components/PeopleList.js';
-// import FormPage from './Components/FormPage';
-// import WelcomePage from './Components/WelcomePage';
-// import ProfilePage from './Components/ProfilePage'; // Import the ProfilePage component
+import FormPage from './Components/FormPage';
+import WelcomePage from './Components/WelcomePage';
+import ProfilePage from './Components/ProfilePage'; // Import the ProfilePage component
 
 function App() {
-  // const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
   const [people, setPeople] = useState([])
 
 
-  // useEffect(() => {
-  //   const timeout = setTimeout(() => {
-  //     setLoading(false);
-  //   }, 3000);
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setLoading(false);
+    }, 3000);
     
 
-  //   return () => clearTimeout(timeout);
-  // }, []);
+    return () => clearTimeout(timeout);
+  }, []);
 
 
   useEffect(() => {
@@ -28,33 +28,28 @@ function App() {
     })
   })
   return(
-    <div>
+    
+
+    <Router>
+      <div className="App">
       <h3>
        <center>Dating APP</center>
       </h3>
       <PeopleList people ={people}/>
-    </div>
-  )
-
-  //   <Router>
-  //     <div className="App">
-  //       <h2>
-  //         Dating APP
-  //       </h2>
-  //       {loading ? (
-  //         <div className='load-container'>LOVETUBE</div>
-  //       ) : (
-  //         <Routes>
-  //           <Route path="/" element={<HomePage />} />
-  //           <Route path="/form" element={<FormPage />} />
-  //           <Route path="/welcome" element={<WelcomePage />} />
-  //           <Route path="/profile" element={<ProfilePage />} /> {/* Add route for ProfilePage */}
-  //         </Routes>
-  //       )}
-  //     </div>
-  //   </Router>
-  // );
+        {loading ? (
+          <div className='load-container'>LOVETUBE</div>
+        ) : (
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/form" element={<FormPage />} />
+            <Route path="/welcome" element={<WelcomePage />} />
+            <Route path="/profile" element={<ProfilePage />} /> {/* Add route for ProfilePage */}
+          </Routes>
+        )}
+      </div>
+    </Router>
   
+      )
 }
 
 export default App;
