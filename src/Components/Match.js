3e40208def5b6172cf65c76e9dce51a43bcf6c3b
edwinsupremacy <edwinmwangi837@gmail.com>
2024-05-09 11:54:27 +0300

    updated Match.css

--- src/Components/Match.js
+++ src/Components/Match.js
@@ -3,20 +3,17 @@ import './Match.css';
 
 function Match({ selectedPeople,onRemove}) {
   const handleCardClick = (person) => {
-    onRemove(person.id);}
+    onRemove(person);}
   return (
     <div className='Match'>
       <h2>Liked People</h2>
       <div className="liked"></div>
       {selectedPeople.map((person) => (
       <div key={person.id} onClick={() => handleCardClick(person)} className="liked-person">
-        
-          <img src="" className="card-img-top" alt="..." />
-          <h3>{person.name}</h3>
+          <h3 className='name'>{person.name}</h3>
           <p>Age:{person.age}</p>
           <p>Gender:{person.gender}</p>
-          <p>Location: {person.location}</p>
-       
+          <p>Location: {person.location}</p> 
         </div>
 
       ))}
